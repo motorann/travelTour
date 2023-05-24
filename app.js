b0462074e61f022fe29255cbcf9fe425c3459691
Anna Motorina <scripa57@gmail.com>
2023-05-24 22:08:59 +0300

    Add EJS, and create first module fortune.js in lib folder

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const app = express();
 
+const fortune = require(__dirname + '/lib/fortune');
+
 const port = process.env.port || 3000;
+app.set('view engine', 'ejs');
+// app.use(express.static(__dirname + 'public'));
 
 app.get('/', (req, res) => {
-  res.type('text/plain');
-  res.send('Homepage');
+  res.type('text/html');
+  res.render('pages/home.ejs');
 });
 
 app.get('/about', (req, res) => {
-  res.type('text/plain');
-  res.send('About page');
+  res.render('pages/about', { todayFortune: fortune() });
 });
 
 app.use((req, res) => {
-  res.type('text/plain');
-  res.status(400);
-  res.send('404 - Not found');
+  res.status(404);
+  res.render('pages/404');
 });
 
 app.use((err, req, res, next) => {
   console.log(err.message);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('pages/500');
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
